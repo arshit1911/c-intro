b1c05cd1949269b60aa930f9bbf982bd82d8f107
arshit1911 <arshitmahajan37@gmail.com>
2020-10-05 10:42:09 +0530

    changes

--- add.c
+++ add.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-  int a, b;
-  printf("Enter number a");  
+int main()
+{
+  int a, b, sum;
+  printf("Enter number a\n");  
   scanf("%d", &a);
 
-  printf("Enter number b");
+  printf("Enter number b\n");
   scanf("%d", &b);
 
+  printf("The sum is %d\n", a+b);
+  
   return 0;
 }
