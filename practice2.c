94cf36a442f4bcdd7794b98311204077050fa46f
arshit1911 <arshitmahajan37@gmail.com>
2020-10-08 01:23:34 +0530

    message

---
+++ practice2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int length = 3, breadth = 5;
+    int area = length * breadth;
+    printf("Area of this rectangle is %d", area);
+    return 0;
+}
--- size2.c
+++ size2.c
@@ -4,7 +4,7 @@ int main()
 {
     int a = 0;
 
-    double a = 0;
+    double A = 0;
     printf("%lf", sizeof(double));
     return 0;
 }
