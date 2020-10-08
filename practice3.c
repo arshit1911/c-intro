71cd448f09162594f55b2efd31726b9c422e8c70
arshit1911 <arshitmahajan37@gmail.com>
2020-10-08 23:50:32 +0530

    message

--- practice3.c
+++ practice3.c
@@ -2,8 +2,11 @@
 
 int main()
 {
-    int radius = 3;
+    int radius;
     float pi = 3.14;
-    printf("The radius of circle is %f", pi * radius * radius);
+    printf("what is the radius of the circle\n");
+    scanf("%d", &radius);
+
+    printf("The area of your circle is %f", pi * radius * radius);
     return 0;
 }
